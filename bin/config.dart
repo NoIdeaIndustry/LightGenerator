cceac26dff81bdf87b2af9acb776a8985c950340
NoIdeaIndustry <contact@noideaindustry.com>
2023-04-01 11:33:29 +0100

    :bricks: Moved 'Config' class

---
+++ bin/config.dart
@@ -0,0 +1,7 @@
+class Config {
+  // the local path from the script parent folder to look for files from
+  static final String kFolderPath = "/files";
+
+  // the url to access files from the internet
+  static final String kHostPath = "https/exemple.com/Updater/myFiles/";
+}
--- bin/main.dart
+++ bin/main.dart
@@ -3,13 +3,7 @@ import 'dart:io';
 import 'package:crypto/crypto.dart';
 import 'package:path/path.dart' as path;
 
-class Config {
-  // the local path from the script parent folder to look for files from
-  static final String kFolderPath = "/files";
-
-  // the url to access files from the internet
-  static final String kHostPath = "https/exemple.com/Updater/myFiles/";
-}
+import 'config.dart';
 
 void main() {
   final localDir = Directory(Directory.current.path);
